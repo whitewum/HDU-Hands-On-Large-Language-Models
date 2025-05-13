8330e2a4017acbb35751411e733ac3ab6c698905
min.wu <50101159+whitewum@users.noreply.github.com>
2025-05-13 14:34:35 +0800

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,33 @@
+from modelscope import AutoModelForCausalLM, AutoTokenizer
+
+model_name = "qwen/Qwen2.5-0.5B-Instruct"
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    torch_dtype="auto",
+    device_map="auto"
+)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+prompt = "对电影评论进行 POSITIVE、NEUTRAL 或 NEGATIVE 分类。评论：“Her”是一部揭示如果人工智能被无限制地发展，人类将走向何方令人不安的研究，我希望有更多像这部杰作一样的电影。情绪："
+messages = [
+    {"role": "system", "content": "You are Qwen, created by Alibaba Cloud. You are a helpful assistant."},
+    {"role": "user", "content": prompt}
+]
+text = tokenizer.apply_chat_template(
+    messages,
+    tokenize=False,
+    add_generation_prompt=True
+)
+model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
+
+generated_ids = model.generate(
+    **model_inputs,
+    max_new_tokens=512
+)
+generated_ids = [
+    output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+]
+
+response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+print(response)
